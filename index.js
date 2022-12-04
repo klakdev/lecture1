d57594ebe7bb9390713b8f20a3504002e7b59e74
Yaki Klein <yaki.klein@intel.com>
2022-12-04 21:31:01 +0200

    use file system

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+import { writeFile, readFile } from 'node:fs';
+import { exit } from 'node:process';
 
-let x = 0;
-while (x < 100) {
-    console.log("x", x++);
+const me = {
+    firstName: "Yaki",
+    lastName: "Klein",
+    age: 37
 }
+
+writeFile("./me.json", JSON.stringify(me), (err) => {
+    if(err) {
+        console.error(err);
+        exit(1);
+    }
+    readFile("./me.json", (err, data) => {
+        if(err) {
+            console.error(err);
+            exit(2);
+        }
+        console.log("%s", data)
+        exit(0);
+    })
+})
